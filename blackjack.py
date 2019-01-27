dc9e91955383a671c00497c6761c59665cc90dbd
Deniz <dzeran@ucvts.org>
2019-01-27 17:49:53 +0000

    the game is finally functional with a weird glitch with the aces

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,4 @@
 import random
-
 class Card:
     def __init__(self,rank, suit):
         self.rank = rank
@@ -13,7 +12,16 @@ class Card:
     def __str__(self):
         return f"{self.rank} of {self.suit}"
 
-
+def play_again():
+        global breaker
+        again = input("do you want to play again [y] or [n]").lower()
+        if again == "y":
+            cleardeck()
+            game()
+        elif again == "n":
+            print("bye")
+            breaker = False
+            return breaker
 
 def make_deck():
     deck = []
@@ -21,7 +29,6 @@ def make_deck():
         for rank in ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king']:
             deck.append(Card(rank,suit))
     return deck
-
 
 def deal(deck,person):
     hand = []
@@ -32,7 +39,7 @@ def deal(deck,person):
         if person == "player":
             print(card)
     if person == "dealer":
-        print(hand[0])
+        print(hand[1])
     return hand
 
 def total_value(hand):
@@ -45,17 +52,14 @@ def total_value(hand):
                     v = int(input("1 or 11?"))
                 card.value = v
             total += card.value
+        print(f"this is the value of the player's hand:{total}")
     elif hand == dealer_hand:
         for card in hand:
             if card.rank == "ace":
                 v= 10
                 card.value = v
             total += card.value
-    if hand == player_hand:
-        print(f"this is the value of the player's hand:{total}")
     return total
-
-
 
 def deckshuffle(deck):
     while True:
@@ -75,13 +79,11 @@ def hit(hand,person):
         for card in hand:
             print(card)
     if person == "dealer":
-        print(hand[0])
-        print(hand[2])
+        for card in hand[0:]:
+            print(card)
     return hand
 
 def results():
-    #global wallet
-    print("results")
     print("This is was your hand:")
     for card in player_hand:
         print(card)
@@ -89,47 +91,35 @@ def results():
     print("This was the dealer's hand:")
     for card in dealer_hand:
         print(card)
-    #print(f"This now your wallet:[wallet]")
     cleardeck()
 
 def blackjack(dealer_hand,player_hand):
-    #global wallet
-    #global bet
     if total_value(dealer_hand) == 21:
-       # wallet -= bet
         results()
         print("The dealer had blackjack. Good game you lose")
+        play_again()
     elif total_value(player_hand) == 21:
-       # wallet += bet
         results()
         print("You had black jack congrats you won")
-
+        play_again()
 
 def score(dealer_hand, player_hand):
-   # global wallet
-   # global bet
     if total_value(player_hand) == 21:
-       # wallet += bet
         results()
-        print("Congratulations! You got a Blackjack!")
+        print("Congratulations! You got Blackjack!")
     elif total_value(dealer_hand) == 21:
-       # wallet -= bet
         results()
-        print("Sorry, you lose. The dealer got a blackjack.")
+        print("Sorry, you lose. The dealer got Blackjack.")
     elif total_value(player_hand) > 21:
-       # wallet -= bet
         results()
         print("Sorry. You busted. You lose.")
     elif total_value(dealer_hand) > 21:
-       # wallet += bet
         results()
         print("Dealer busts. You win!")
     elif total_value(dealer_hand) > total_value(player_hand):
-       # wallet -= bet
         results()
         print("Sorry. Your score is lower than the dealers. You lose")
     elif total_value(player_hand) > total_value(dealer_hand):
-       # wallet += bet
         results()
         print ("Congratulations. Your score is higher than the dealer. You win")
     elif total_value(player_hand) == total_value(dealer_hand):
@@ -137,15 +127,17 @@ def score(dealer_hand, player_hand):
         print("It was a draw, Dealer wins")
 
 def bust():
+    global breaker
     if total_value(player_hand) > 21:
         results()
         print("Player Busts. You lose")
+        play_again()
     elif total_value(dealer_hand) > 21:
         results()
         print("Dealer Busts. You win")
+        play_again()
     else:
         pass
-
 
 def cleardeck():
     garbage = []
@@ -157,16 +149,28 @@ def cleardeck():
         dealer_hand.pop(dealer_hand.index(card))
 
 def game():
+    print(" ")
     print("Welcome to blackjack. Your goal is to get your hand to get as close to 21 or to be 21. But if you go over you go bust.")
+    print(" ")
+    player_input = input("Do you want to review the basics of blackjack, [y]es or [n]o: ").lower()
+    print(" ")
+    if player_input == "y":
+        print("Now here are the basics,")
+        print("     -All face cards equal 10 and an Ace can either equal 1 or 11 it is your choice")
+        print("     -All number cards equal their number")
+        print("     -When you are first delt the cards, your hand will show while the dealers hand has only one card showing")
+        print("     -During the game you can hit, stay, or fold")
+        print("         -Hitting adds a new deck to your hand, but it can be almost any card from an ace to king, so hit with caution")
+        print("         -Staying means you dont want any more cards and youa and the dealer show cards and sees who wins")
+        print("         -Folding means you gave up and the dealer automatically wins")
+        print("     -To win the game you just have to have a higher value than the dealer or to get 21, but you go over you lose")
+        print("     -If the game ends in a draw the dealer automatically wins")
+    print(" ")
+    global breaker
     choice = 0
     global deck
-   # global wallet
-   # global bet
     global dealer_hand
     global player_hand
-   # wallet = 0
-   # wallet = int(input("How much do you want to bring to the table:"))
-   # bet = int(input("how much would you like to bet:"))
     breaker = True
     while breaker == True:
         deck = make_deck()
@@ -176,8 +180,8 @@ def game():
         print("This is the dealer's hand:")
         dealer_hand= deal(deck, "dealer")
         blackjack(dealer_hand, player_hand)
-        while choice != "q":
-            choice = input("Do you want to [H]it, [S]tand, or [Q]uit: ").lower()
+        while breaker == True:
+            choice = input("Do you want to [H]it, [S]tand, or [F]old: ").lower()
             if choice == "h":
                 print("This is your new hand")
                 player_hand = hit(player_hand,"player")
@@ -190,14 +194,10 @@ def game():
                     hit(dealer_hand,"dealer")
                 bust()
                 score(dealer_hand, player_hand)
-                again = input("do you want to play again [y] or [n]").lower()
-                if again == "y":
-                    cleardeck()
-                    game()
-                elif choice == "q" or again == "n":
-                    print("bye")
-                    break
-                    breaker = False
-                    return breaker
+                play_again()
+            elif choice == "f":
+                print("bye")
+                breaker = False
+
 
 game()
